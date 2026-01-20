5788d05049dee6a7bef903ca40ff63657652a25c
Nilsen Martinez <nilsenmr@gmail.com>
2026-01-19 22:26:36 -0400

    update cors

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,21 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import cors from 'cors';
 import prendasRouter from './modules/prendas/router';
 
 dotenv.config();
 const app = express();
+
 app.use(express.json());
 
+// Configuración de CORS
+app.use(cors({
+  origin: "http://localhost:5173", // el puerto donde corre Vite
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
+}));
+
+// Tus rutas
 app.use('/api/prendas', prendasRouter);
 
 const PORT = process.env.PORT || 3001;
